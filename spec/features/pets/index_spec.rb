1e6c43e223416127d9ba17018474d68b569e5358
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-03 16:37:27 -0700

    FactoryBot refactor

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -2,54 +2,29 @@ require 'rails_helper'
 
 describe "As a user, when I visit the pet index page," do
   before :each do
-    @shelter_1 = Shelter.create!(
-      name:     "Winterfell",
-      address:  "6303 W Exposition Ave",
-      city:     "Lakewood",
-      state:    "CO",
-      zip:      "80226"
-    )
-    @shelter_2 = Shelter.create!(
-      name:     "Sunspear",
-      address:  "12322 Bohannon Blvd",
-      city:     "Orlando",
-      state:    "FL",
-      zip:      "32824"
-    )
+    shelters = create_list(:random_shelter, 2)
 
-    @pet_1 = @shelter_1.pets.create!(
-      image:            "https://www.allthingsdogs.com/wp-content/uploads/2018/12/Husky-Names-Feature.jpg",
-      name:             "Seamus",
-      approximate_age:  8,
-      sex:              "male",
-      description:      "Winter is Coming; dog"
-    )
-    @pet_2 = @shelter_2.pets.create!(
-      image:            "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234710/Chihuahua-On-White-03.jpg",
-      name:             "Pepper",
-      approximate_age:  4,
-      sex:              "female",
-      description:      "Unbowed, Unbent, Unbroken; dog"
-    )
+    @pet_1 = create(:random_pet, shelter: shelters[0])
+    @pet_2 = create(:random_pet, shelter: shelters[1])
 
     visit "/pets"
   end
 
   it "I can see all pets and their image, name, age, sex, name of shelter" do
     within "#pet-#{@pet_1.id}" do
-      expect(page).to have_css "img[src *= 'Husky-Names-Feature.jpg']"
-      expect(page).to have_content "Seamus"
-      expect(page).to have_content 8
-      expect(page).to have_content "male"
-      expect(page).to have_content "Winterfell"
+      expect(page).to have_css "img[src = '#{@pet_1.image}']"
+      expect(page).to have_content @pet_1.name
+      expect(page).to have_content @pet_1.approximate_age
+      expect(page).to have_content @pet_1.sex
+      expect(page).to have_content @pet_1.shelter.name
     end
-
+    
     within "#pet-#{@pet_2.id}" do
-      expect(page).to have_css "img[src *= 'Chihuahua-On-White-03.jpg']"
-      expect(page).to have_content "Pepper"
-      expect(page).to have_content 4
-      expect(page).to have_content "female"
-      expect(page).to have_content "Sunspear"
+      expect(page).to have_css "img[src = '#{@pet_2.image}']"
+      expect(page).to have_content @pet_2.name
+      expect(page).to have_content @pet_2.approximate_age
+      expect(page).to have_content @pet_2.sex
+      expect(page).to have_content @pet_2.shelter.name
     end
   end
 
@@ -69,7 +44,7 @@ describe "As a user, when I visit the pet index page," do
 
   it "I can navigate to a shelter on the page" do
     within "#pet-#{@pet_1.id}" do
-      click_link "#{@pet_1.shelter_name}"
+      click_link "#{@pet_1.shelter.name}"
     end
     expect(current_path).to eq "/shelters/#{@pet_1.shelter_id}"
   end
@@ -82,10 +57,10 @@ describe "As a user, when I visit the pet index page," do
   end
 
   it "I can see the pets by adoptable status" do
-    expect(page.body.index("Seamus")).to be < page.body.index("Pepper")
+    expect(page.body.index(@pet_1.name)).to be < page.body.index(@pet_2.name)
     visit "/pets/#{@pet_1.id}"
     click_link "Change to Adoption Pending"
     visit "/pets"
-    expect(page.body.index("Pepper")).to be < page.body.index("Seamus")
+    expect(page.body.index(@pet_2.name)).to be < page.body.index(@pet_1.name)
   end
 end
