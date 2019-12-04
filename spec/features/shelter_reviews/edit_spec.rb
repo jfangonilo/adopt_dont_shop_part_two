5378cfce3a19f6247f2e2d25add69dd953e16ec8
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 13:49:32 -0700

    Recommit changes Jomah made, accidentally unstaged

--- spec/features/shelter_reviews/edit_spec.rb
+++ spec/features/shelter_reviews/edit_spec.rb
@@ -19,9 +19,24 @@ describe "shelter edit page" do
     click_button "Update"
 
     expect(current_path).to eq "/shelters/#{@review.shelter.id}"
-    expect(page).to have_content @review.title
-    expect(page).to have_content @review.rating
-    expect(page).to have_content @review.content
-    expect(page).to have_css "img[src = '#{@review.picture}']"
+
+    expect(page).to have_content title
+    expect(page).to have_content content
+    expect(page).to have_css "img[src = '#{picture}']"
+    
+    expect(page).not_to have_content @review.title
+    expect(page).not_to have_content @review.content
+    expect(page).not_to have_css "img[src = '#{@review.picture}']"
+  end
+
+  it "flashes a message when user doesn't fill out a field" do
+    click_button "Update"
+
+    expect(page).to have_content "Please fill out all fields"
+    expect(page).to have_button "Update"
+
+    click_button "Update"
+    expect(page).to have_content "Please fill out all fields"
+    expect(page).to have_button "Update"
   end
 end
