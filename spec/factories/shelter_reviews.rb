7b2667b0cd5a1ed8f6a320ea73b14888c13d18ff
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 12:08:14 -0700

    Refactor with sequence so content is unique

--- spec/factories/shelter_reviews.rb
+++ spec/factories/shelter_reviews.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :random_shelter_review, class: ShelterReview do
     sequence(:title)               {|n| "#{Faker::TvShows::MichaelScott.quote}#{n}"}
     rating              {[1,2,3,4,5].sample }
-    content             {Faker::TvShows::TheITCrowd.quote}
+    sequence(:content)             {|n| "#{Faker::TvShows::TheITCrowd.quote}#{n}"}
     sequence(:picture)  {|n| "https://picsum.photos/400/300?image=#{n}"}
 
     association         :shelter, factory: :random_shelter
