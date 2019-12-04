431ca03cff55319f45b504ffd9f5c2702cf6ee65
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 14:52:29 -0700

    Redirect to pet show page after add to favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,9 @@
 class FavoritesController < ApplicationController
 
   def update
+    pet = Pet.find(params[:pet_id])
+    redirect_to "/pets/#{pet.id}"
+    flash[:favorited] = "#{pet.name} has been added to your favorites!"
   end
 
 end
