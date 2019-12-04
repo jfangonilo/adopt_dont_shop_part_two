9121dd4f4144eecd576f450de5078a1b1e3bb75b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-03 19:54:54 -0700

    Rename route ids to be more explicirt

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
   delete '/pets/:id', to: 'pets#destroy'
   patch '/pets/:id/pending', to: 'pets#toggle_adoptable'
 
-  get '/shelters/:id/shelter_reviews/:shelter_review_id/edit', to: 'shelter_reviews#edit'
+  get '/shelters/:shelter_id/shelter_reviews/:shelter_review_id/edit', to: 'shelter_reviews#edit'
 
 end
