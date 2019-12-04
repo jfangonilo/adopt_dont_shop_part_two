3b6c224d8b3a7d00900ab62416577ee03a078377
Madelyn Romero <m.romero013@gmail.com>
2019-12-03 21:21:01 -0700

    Destroy shelter review in destroy action

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -18,6 +18,7 @@ class ShelterReviewsController < ApplicationController
 
   def destroy
     shelter_id = params[:shelter_id]
+    ShelterReview.destroy(params[:shelter_review_id])
     redirect_to "/shelters/#{shelter_id}"
   end
 
