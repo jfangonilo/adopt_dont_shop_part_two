3afaadb2ba8dd1ed8369dafa3a20c1ea439f32a6
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 13:02:19 -0700

    Refactor, keep address, city, state, zip without sequence

--- spec/factories/shelters.rb
+++ spec/factories/shelters.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :random_shelter, class: Shelter do
     sequence(:name)    {|n| "#{Faker::TvShows::GameOfThrones.city}#{n}"}
-    sequence(:address) {|n| "#{Faker::Address.street_address}#{n}"}
-    sequence(:city)    {|n| "#{Faker::Address.city}#{n}"}
-    state   {Faker::Address.state}
-    zip     {Faker::Address.zip}
+    address            {Faker::Address.street_address}
+    city               {Faker::Address.city}
+    state              {Faker::Address.state}
+    zip                {Faker::Address.zip}
   end
 end
