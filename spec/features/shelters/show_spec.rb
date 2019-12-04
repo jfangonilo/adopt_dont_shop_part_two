f5f76d95db38ad5299875407cf50f8709555e691
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 11:23:08 -0700

    Add test for add new review from shelter show page

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -37,6 +37,32 @@ describe "As a visitor, when I visit /shelters/:id," do
     expect(current_path).to eq "/shelters/#{@shelter_1.id}/pets"
   end
 
+  it "I can create a new shelter review" do
+    click_link "Add a Review"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/shelter_reviews/new")
+    expect(page).to have_content("Add a Review")
+
+    review_title = "Best shelter ever!"
+    review_rating = 5
+    review_content = "Adopted the best puppy! Only got bit once!"
+    review_picture = "https://images.unsplash.com/photo-1506917728037-b6af01a7d403?ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80"
+
+    fill_in "Title", with: review_title
+    fill_in "Rating", with: review_rating
+    fill_in "Content", with: review_content
+    fill_in "Picture", with: review_picture
+
+    click_button "Submit Review"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+
+    expect(page).to have_content(review_title)
+    expect(page).to have_content(review_rating)
+    expect(page).to have_content(review_content)
+    expect(page).to have_css("img[src = '#{review_picture}']")
+  end
+
   it "I can see all shelter reviews and their traits" do
     @reviews  = create_list(:random_shelter_review, 3, shelter: @shelter_1)
 
@@ -64,7 +90,7 @@ describe "As a visitor, when I visit /shelters/:id," do
     within "#review-#{@reviews[0].id}" do
       click_link "Delete Review"
     end
-    
+
       expect(current_path).to eq "/shelters/#{@shelter_1.id}"
       expect(page).not_to have_content(@reviews[0].title)
       expect(page).not_to have_content(@reviews[0].content)
