bd73b21e518dcd3329e12c66bd3c545d3cf8a0b6
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 12:04:17 -0700

    Refactor description to sequence so always unique

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -1,14 +1,11 @@
 FactoryBot.define do
   factory :random_pet, class: Pet do
-    sequence(:name)   {|n| "#{Faker::Creature::Cat.name}#{n}"}
-    sequence(:image)  {|n| "http://lorempixel.com/400/300/cats/#{n}"}
-    approximate_age   {rand(20)}
-    sex               {Faker::Gender.binary_type}
-    description      {Faker::Movies::StarWars.wookiee_sentence}
+    sequence(:name)           {|n| "#{Faker::Creature::Cat.name}#{n}"}
+    sequence(:image)          {|n| "http://lorempixel.com/400/300/cats/#{n}"}
+    approximate_age           {rand(20)}
+    sex                       {Faker::Gender.binary_type}
+    sequence(:description)    {|n| "#{Faker::Movies::StarWars.wookiee_sentence}#{n}"}
 
     association       :shelter, factory: :random_shelter
   end
 end
-
-
-# sequence(:description)       {|n| "#{Faker::Movies::StarWars.wookiee_sentence}#{n}"}
