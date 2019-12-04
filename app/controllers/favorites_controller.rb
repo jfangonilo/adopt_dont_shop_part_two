bc7e5f21b5c4ae04cee1b718ed52f3380a93c4f3
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 14:53:24 -0700

    Add flash message to update action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
     pet = Pet.find(params[:pet_id])
     redirect_to "/pets/#{pet.id}"
     flash[:favorited] = "#{pet.name} has been added to your favorites!"
+    
   end
 
 end
