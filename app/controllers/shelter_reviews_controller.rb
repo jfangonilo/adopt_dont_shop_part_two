bf19f48eda145d3bc41e6a55409c83323a4a1e52
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 13:43:03 -0700

    Add initial if/else block for flash message for new shelter review

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -5,15 +5,34 @@ class ShelterReviewsController < ApplicationController
   end
 
   def create
+    # @shelter_id = params[:shelter_id]
     shelter = Shelter.find(params[:shelter_id])
-    shelter_review = shelter.shelter_reviews.create(shelter_review_params)
-    shelter_review.save
+    shelter_review = shelter.shelter_reviews.new(shelter_review_params)
 
-    redirect_to "/shelters/#{shelter.id}"
+    if shelter_review.save
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:notice] = "Please fill out all fields"
+      render :new
+    end
   end
 
   def edit
+    @review_id = params[:shelter_review_id]
     @shelter_id = params[:shelter_id]
+  end
+
+  def update
+    review = ShelterReview.find(params[:shelter_review_id])
+    review.update(shelter_review_params)
+    if review.save
+      redirect_to "/shelters/#{review.shelter_id}"
+    else
+      @review_id = params[:shelter_review_id]
+      @shelter_id = params[:shelter_id]
+      flash.now[:notice] = "Please fill out all fields"
+      render :edit
+    end
   end
 
   def destroy
