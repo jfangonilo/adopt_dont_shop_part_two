4b440333afaccd77f211c31ecd5ca72cc1a2a540
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 12:59:37 -0700

    Refactor city to sequence so unique

--- spec/factories/shelters.rb
+++ spec/factories/shelters.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :random_shelter, class: Shelter do
     sequence(:name)    {|n| "#{Faker::TvShows::GameOfThrones.city}#{n}"}
     sequence(:address) {|n| "#{Faker::Address.street_address}#{n}"}
-    city    {Faker::Address.city}
+    sequence(:city)    {|n| "#{Faker::Address.city}#{n}"}
     state   {Faker::Address.state}
     zip     {Faker::Address.zip}
   end
