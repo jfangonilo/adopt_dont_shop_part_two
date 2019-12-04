24a392226f7abc64e88f2d8408eccd26c2306b1e
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 12:08:51 -0700

    Delete spacing so all in line

--- spec/factories/shelter_reviews.rb
+++ spec/factories/shelter_reviews.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :random_shelter_review, class: ShelterReview do
-    sequence(:title)               {|n| "#{Faker::TvShows::MichaelScott.quote}#{n}"}
+    sequence(:title)    {|n| "#{Faker::TvShows::MichaelScott.quote}#{n}"}
     rating              {[1,2,3,4,5].sample }
-    sequence(:content)             {|n| "#{Faker::TvShows::TheITCrowd.quote}#{n}"}
+    sequence(:content)  {|n| "#{Faker::TvShows::TheITCrowd.quote}#{n}"}
     sequence(:picture)  {|n| "https://picsum.photos/400/300?image=#{n}"}
 
     association         :shelter, factory: :random_shelter
