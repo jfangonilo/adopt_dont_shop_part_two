f13fdf65a2bdff1dfbb0fe0835956f44fc9f38c1
Madelyn Romero <m.romero013@gmail.com>
2019-12-04 12:03:06 -0700

    Refactor pet name to sequence so will be unique

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -1,11 +1,14 @@
 FactoryBot.define do
   factory :random_pet, class: Pet do
-    name              {Faker::Creature::Cat.name}
+    sequence(:name)   {|n| "#{Faker::Creature::Cat.name}#{n}"}
     sequence(:image)  {|n| "http://lorempixel.com/400/300/cats/#{n}"}
     approximate_age   {rand(20)}
     sex               {Faker::Gender.binary_type}
-    description       {Faker::Movies::StarWars.wookiee_sentence}
+    description      {Faker::Movies::StarWars.wookiee_sentence}
 
     association       :shelter, factory: :random_shelter
   end
 end
+
+
+# sequence(:description)       {|n| "#{Faker::Movies::StarWars.wookiee_sentence}#{n}"}
