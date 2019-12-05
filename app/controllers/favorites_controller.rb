e3df406924475e3eea80d40f6db0500eafd8a197
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-04 19:29:08 -0700

    Add cookies to update action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,11 @@ class FavoritesController < ApplicationController
 
   def update
     pet = Pet.find(params[:pet_id])
-    redirect_to "/pets/#{pet.id}"
-    flash[:favorited] = "#{pet.name} has been added to your favorites!"
-    
-  end
+    cookies[:favorites] ||= []
+    cookies[:favorites] << pet
 
+    binding.pry
+    flash[:favorited] = "#{pet.name} has been added to your favorites!"
+    redirect_to "/pets/#{pet.id}"
+  end
 end
