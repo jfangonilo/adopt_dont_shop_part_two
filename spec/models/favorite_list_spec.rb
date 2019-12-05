c8edb8fb3df2cfaf4134cf105cbd07f6069641c1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-04 20:06:24 -0700

    Add test to add pet

--- spec/models/favorite_list_spec.rb
+++ spec/models/favorite_list_spec.rb
@@ -2,14 +2,22 @@ require "rails_helper"
 
 describe FavoriteList do
   before :each do
-    @cats = create_list(:random_pet, 5)
+    cats = create_list(:random_pet, 5)
+    @list = FavoriteList.new(cats)
   end
 
   describe "#total_count" do
     it "calculates total number of favorites" do
-      list = FavoriteList.new(@cats)
+      expect(@list.total_count).to eq 5
+    end
+  end
 
-      expect(list.total_count).to eq 5
+  describe "#add_pet" do
+    it "adds a pet to the list" do
+      new_cat = create(:random_pet)
+      @list.add_pet(new_cat)
+
+      expect(@list.total_count).to eq 6
     end
   end
 end
