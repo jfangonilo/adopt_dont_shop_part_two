4768918684b7dc3281f0cfc9d3abe549c0d12421
Madelyn Romero <m.romero013@gmail.com>
2019-12-05 14:30:36 -0700

    Fix typo

--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe Favorites do
       list = Favorites.new
       list.add_pet(1)
 
-      expect(list.contents).to eq({1 => 1})
+      expect(list.contents).to eq({'1' => 1})
     end
   end
 
   describe "#total_count" do
     it "can calculate total number of pets in favorites" do
       list = Favorites.new({
-        1 => 1,
-        2 => 1,
-        3 => 1,
-        4 => 1,
-        5 => 1,
+        '1' => 1,
+        '2' => 1,
+        '3' => 1,
+        '4' => 1,
+        '5' => 1,
         })
 
       expect(list.total_count).to eq(5)
