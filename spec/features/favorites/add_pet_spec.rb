3f1cc5ac29254b8df561a12f13febcd65ef2c103
Madelyn Romero <m.romero013@gmail.com>
2019-12-05 12:29:05 -0700

    Add test for nav bar count

--- spec/features/favorites/add_pet_spec.rb
+++ spec/features/favorites/add_pet_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe "When a user adds a pet to their favorites" do
 
     expect(page).to have_content("#{pet.name} has been added to your favorites!")
   end
+
+  it "displays count of all favorites" do
+    pet_1 = create(:random_pet)
+    visit "/pets/#{pet_1.id}"
+    click_button "Add #{pet_1.name} to Favorites"
+
+    with ".navbar" do
+      expect(page).to have_content("Favorites: 1")
+    end
+
+    pet_2 = create(:random_pet)
+    visit "/pets/#{pet_2.id}"
+    click_button "Add #{pet_2.name} to Favorites"
+
+    with ".navbar" do
+      expect(page).to have_content("Favorites: 2")
+    end
+  end
 end
