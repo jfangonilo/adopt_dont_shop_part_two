3549adcf05aa3847744eeec991296621bc4974cf
Madelyn Romero <m.romero013@gmail.com>
2019-12-05 14:27:04 -0700

    Add test for adding pet to favorites

--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Favorites do
 
+  describe "#add_pet" do
+    it "adds a pet to favorites" do
+      list = Favorites.new
+      list.add_pet(1)
+
+      expect(list.contents).to eq({1 => 1})
+    end
+  end
+
   describe "#total_count" do
     it "can calculate total number of pets in favorites" do
       list = Favorites.new({
