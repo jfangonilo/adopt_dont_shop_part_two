3bd715566e42174494eef788c032a0b17e3044c1
Madelyn Romero <m.romero013@gmail.com>
2019-12-06 15:48:29 -0700

    Change order of tests

--- spec/features/favorites/destroy_spec.rb
+++ spec/features/favorites/destroy_spec.rb
@@ -1,24 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "a user can remove individual pets from favorites" do
-  it "add to favorites button appears when pet not in favorites and remove from favorites button appears when pet in favorites" do
-    pet_1 = create(:random_pet)
 
-    visit "/pets/#{pet_1.id}"
-    expect(page).not_to have_content("Remove #{pet_1.name} from Favorites")
-
-    click_button "Add #{pet_1.name} to Favorites"
-    expect(current_path).to eq("/pets/#{pet_1.id}")
-    expect(page).not_to have_button("Add #{pet_1.name} to Favorites")
-    expect(page).to have_button("Remove #{pet_1.name} from Favorites")
-
-    click_button "Remove #{pet_1.name} from Favorites"
-    expect(current_path).to eq("/pets/#{pet_1.id}")
-    expect(page).not_to have_button("Remove #{pet_1.name} from Favorites")
-    expect(page).to have_button("Add #{pet_1.name} to Favorites")
-  end
-
-  xit "remove from favorites button removes pet from favorites" do
+  it "remove from favorites button removes pet from favorites" do
     pet_1 = create(:random_pet)
 
     visit "/pets/#{pet_1.id}"
@@ -34,6 +18,23 @@ RSpec.describe "a user can remove individual pets from favorites" do
     within ".topnav" do
       expect(page).to have_content("Favorites: 0")
     end
+  end
+
+  it "add to favorites button appears when pet not in favorites and remove from favorites button appears when pet in favorites" do
+    pet_1 = create(:random_pet)
+
+    visit "/pets/#{pet_1.id}"
+    expect(page).not_to have_content("Remove #{pet_1.name} from Favorites")
+
+    click_button "Add #{pet_1.name} to Favorites"
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+    expect(page).not_to have_button("Add #{pet_1.name} to Favorites")
+    expect(page).to have_button("Remove #{pet_1.name} from Favorites")
+
+    click_button "Remove #{pet_1.name} from Favorites"
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+    expect(page).not_to have_button("Remove #{pet_1.name} from Favorites")
+    expect(page).to have_button("Add #{pet_1.name} to Favorites")
   end
 
   xit "remove from favorites button redirects to pet show page and displays flash message" do
