77586f9a0f8443ee8478bc888387e295499f3a83
Madelyn Romero <m.romero013@gmail.com>
2019-12-06 15:19:59 -0700

    Refactor destroy test to cover all of user story

--- spec/features/favorites/destroy_spec.rb
+++ spec/features/favorites/destroy_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "a user can remove individual pets from favorites" do
-  it "button to remove pet appears if pet is in favorites and add to favorites button does not appear" do
+  it "add to favorites button appears when pet not in favorites and remove from favorites button appears when pet in favorites" do
     pet_1 = create(:random_pet)
 
     visit "/pets/#{pet_1.id}"
-    click_button "Add #{pet_1.name} to Favorites"
+    expect(page).not_to have_content("Remove #{pet_1.name} from Favorites")
 
+    click_button "Add #{pet_1.name} to Favorites"
     expect(current_path).to eq("/pets/#{pet_1.id}")
     expect(page).not_to have_button("Add #{pet_1.name} to Favorites")
     expect(page).to have_button("Remove #{pet_1.name} from Favorites")
+
+    click_button "Remove #{pet_1.name} from Favorites"
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+    expect(page).not_to have_button("Remove #{pet_1.name} from Favorites")
+    expect(page).to have_button("Add #{pet_1.name} to Favorites")
   end
 
   xit "remove from favorites button removes pet from favorites" do
@@ -17,14 +23,24 @@ RSpec.describe "a user can remove individual pets from favorites" do
 
     visit "/pets/#{pet_1.id}"
     click_button "Add #{pet_1.name} to Favorites"
-
+    within ".topnav" do
+      expect(page).to have_content("Favorites: 1")
+    end
     click_button "Remove #{pet_1.name} from Favorites"
-
-    expect(page).not_to have_button("Remove #{pet_1.name} from Favorites")
-    expect(page).to have_button("Add #{pet_1.name} to Favorites")
 
     visit "/favorites"
     expect(page).not_to have_content("#{pet_1.name}")
     expect(page).not_to have_css("img[src = '#{pet_1.image}']")
+    within ".topnav" do
+      expect(page).to have_content("Favorites: 0")
+    end
+  end
+
+  xit "remove from favorites button redirects to pet show page and displays flash message" do
+
+  end
+
+  xit "remove from favorites decreases nav favorites counter by one" do
+
   end
 end
