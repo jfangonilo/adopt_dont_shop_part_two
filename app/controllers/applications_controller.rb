58494bd791e909d02cb8aebdb4794bfd8c34f01d
Madelyn Romero <m.romero013@gmail.com>
2019-12-06 20:35:20 -0700

    Create instance variable to pass favorite pets to view

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationsController < ApplicationController
 
   def new
+    @favorite_pets = favorites.contents.reduce([]) do |acc, (id, value)|
+      acc << Pet.find(id)
+    end
   end
-
 end
