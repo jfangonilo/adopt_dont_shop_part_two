edcfa725447a2f56e9428dd29f69ed40dfc0996d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-06 18:35:53 -0700

    Add validation tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,14 @@ describe Application do
     it {should have_many :pet_applications}
     it {should have_many(:pets).through(:pet_applications)}
   end
+
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+    it {should validate_presence_of :phone_number}
+    it {should validate_presence_of :description}
+  end
 end
