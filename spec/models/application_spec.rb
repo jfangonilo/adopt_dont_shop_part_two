808b72e07ee8a11d6a03b5f86196c2307b510b4e
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-06 18:33:03 -0700

    Add test for relationship with pets

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 describe Application do
   describe "relationships" do
     it {should have_many :pet_applications}
+    it {should have_many(:pets).through(:pet_applications)}
   end
 end
