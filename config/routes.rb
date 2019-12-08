b46121821a3d2498137fc99358d59169906df58f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-07 17:46:17 -0700

    Remove deprecated route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
-  get '/shelters/sort', to: 'shelters#sort'
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
