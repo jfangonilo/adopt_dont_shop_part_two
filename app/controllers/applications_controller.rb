25d76c1582835706e5d53a3c10ab176e5fd8920c
Madelyn Romero <m.romero013@gmail.com>
2019-12-08 14:15:56 -0700

    Add logic to delete pet from favorites after on application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -10,10 +10,13 @@ class ApplicationsController < ApplicationController
       pets = Pet.find(params[:pets_applied_for])
       redirect_to '/favorites'
       messages = []
-      pets.each do |pet|
-        messages <<  "Application sent for #{pet.name}!"
-        flash[:submit] = messages.join(' ')
+        pets.each do |pet|
+          messages <<  "Application sent for #{pet.name}!"
+          flash[:submit] = messages.join(' ')
+        end
+      pet_ids = params[:pets_applied_for]
+      pet_ids.each do |pet_id|
+        favorites.contents.delete(pet_id)
       end
-
     end
 end
