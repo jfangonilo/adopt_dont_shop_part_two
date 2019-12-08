a3d4b3bd49eb0ad8754eccf8faea57cee3dfaaee
Madelyn Romero <m.romero013@gmail.com>
2019-12-08 12:40:55 -0700

    Create flash message for multiple pets, using each block, messy and presenting in array
    Co-Authored-By: Jomah Fangonilo <jomah.fangonilo@gmail.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,9 +4,15 @@ class ApplicationsController < ApplicationController
     @favorite_pets = favorites.contents.reduce([]) do |acc, (id, value)|
       acc << Pet.find(id)
     end
+  end
 
     def create
+      pets = Pet.find(params[:pets_applied_for])
       redirect_to '/favorites'
+      flash[:submit] ||= []
+      pets.each do |pet|
+        flash[:submit] <<  "Application sent for #{pet.name}!"
+      end
+      flash[:submit]
     end
-  end
 end
