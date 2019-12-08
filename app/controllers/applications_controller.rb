4d03c8923f26c132349a15b5fe2c95a0e62de35c
Madelyn Romero <m.romero013@gmail.com>
2019-12-08 15:36:12 -0700

    Add if else block for deleting pets only if application created

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,19 +7,25 @@ class ApplicationsController < ApplicationController
   end
 
     def create
-      app = Application.create(application_params)
       pets = Pet.find(params[:pets_applied_for])
-      app.pets << pets
+      app = Application.new(application_params)
+
+      if app.save || params[:pets_applied_for] == []
+        pet_ids = params[:pets_applied_for]
+        pet_ids.each do |pet_id|
+          favorites.delete_pet(pet_id)
+        end
+        app.pets << pets
+      else
+        flash[:notice] = "Please fill out all fields!"
+      end
+
       redirect_to '/favorites'
       messages = []
         pets.each do |pet|
           messages <<  "Application sent for #{pet.name}!"
           flash[:submit] = messages.join(' ')
         end
-      pet_ids = params[:pets_applied_for]
-      pet_ids.each do |pet_id|
-        favorites.delete_pet(pet_id)
-      end
     end
 
   private
