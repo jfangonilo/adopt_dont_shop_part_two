725a9cf44e0368f0673a70b8ffd3ae6cb457c764
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-08 18:36:58 -0700

    Add test for no button if no favorites

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,6 +1,67 @@
 require "rails_helper"
 
 describe "new application" do
+  it "doesn't have a link to start adoption process if no pets have been added to favorites" do
+    visit "/favorites"
+    expect(page).not_to have_button "Start Adoption Application"
+  end
+
+  it "doesn't accept invalid application (no pets selected/fields missed)" do
+    pet_1 = create(:random_pet)
+    pet_2 = create(:random_pet)
+    pet_3 = create(:random_pet)
+
+    visit "/pets/#{pet_1.id}"
+    click_button "Add #{pet_1.name} to Favorites"
+    visit "/pets/#{pet_2.id}"
+    click_button "Add #{pet_2.name} to Favorites"
+    visit "/pets/#{pet_3.id}"
+    click_button "Add #{pet_3.name} to Favorites"
+
+    visit "/favorites"
+    click_button "Start Adoption Application"
+    click_button "Submit Application"
+
+    expect(current_path).to eq "/applications/new"
+    expect(page).to have_content "Please fill out all fields!"
+  end
+  
+  it "doesn't accept invalid application (no pets/all fields filled)" do
+    pet_1 = create(:random_pet)
+    pet_2 = create(:random_pet)
+    pet_3 = create(:random_pet)
+    
+    visit "/pets/#{pet_1.id}"
+    click_button "Add #{pet_1.name} to Favorites"
+    visit "/pets/#{pet_2.id}"
+    click_button "Add #{pet_2.name} to Favorites"
+    visit "/pets/#{pet_3.id}"
+    click_button "Add #{pet_3.name} to Favorites"
+    
+    visit "/favorites"
+    click_button "Start Adoption Application"
+    
+    name          = "Jomah"
+    address       = "6303 W Exposition Ave"
+    city          = "Lakewood"
+    state         = "CO"
+    zip           = "80226"
+    phone_number  = "4079274443"
+    description   = "I love all animals and will give the goodest boys all the belly rubs"
+    
+    fill_in "name",         with: name
+    fill_in "address",      with: address
+    fill_in "city",         with: city
+    fill_in "state",        with: state
+    fill_in "zip",          with: zip
+    fill_in "phone_number", with: phone_number
+    fill_in "description",  with: description
+    click_button "Submit Application"
+    
+    expect(current_path).to eq "/applications/new"
+    expect(page).to have_content "Please fill out all fields!"
+  end
+
   it "has a link to application form from favorites" do
     pet_1 = create(:random_pet)
     pet_2 = create(:random_pet)
