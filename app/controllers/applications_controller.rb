2da1fe8068e94a0560f15c5fcd66bba7172b3361
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-08 18:57:31 -0700

    Add functionality for sad paths on create

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationsController < ApplicationController
+  before_action :require_pets, only: [:create]
 
   def new
     @favorite_pets = favorites.contents.reduce([]) do |acc, (id, value)|
@@ -6,31 +7,35 @@ class ApplicationsController < ApplicationController
     end
   end
 
-    def create
-      pets = Pet.find(params[:pets_applied_for])
-      app = Application.new(application_params)
+  def create
+    pet_ids = params[:pets_applied_for]
+    pets = Pet.find(pet_ids)
+    app = Application.new(application_params)
 
-      if app.save || params[:pets_applied_for] == []
-        pet_ids = params[:pets_applied_for]
-        pet_ids.each do |pet_id|
-          favorites.delete_pet(pet_id)
-        end
-        app.pets << pets
-      else
-        flash[:notice] = "Please fill out all fields!"
-      end
-
+    if app.save
+      favorites.delete_pets(pet_ids)
+      app.pets << pets
       redirect_to '/favorites'
       messages = []
-        pets.each do |pet|
-          messages <<  "Application sent for #{pet.name}!"
-          flash[:submit] = messages.join(' ')
-        end
+      pets.each do |pet|
+        messages <<  "Application sent for #{pet.name}!"
+        flash[:submit] = messages.join(' ')
+      end
+    else
+      flash[:notice] = "Please fill out all fields!"
+      redirect_to "/applications/new"
     end
+  end
 
-  private
+private
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
+  end
 
-    def application_params
-      params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
+  def require_pets
+    if params[:pets_applied_for].nil?
+      flash[:notice] = "Please fill out all fields!"
+      redirect_to "/applications/new"
     end
+  end
 end
