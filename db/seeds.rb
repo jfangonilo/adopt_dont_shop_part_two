ced37c4dc30e20cccb873b02024004f2999c2283
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-08 20:13:16 -0700

    Add application factory to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
-shelters = FactoryBot.create_list(:random_shelter, 3)
-shelters.each { |shelter| FactoryBot.create_list(:random_pet, 3, shelter: shelter) }
-shelters.each { |shelter| FactoryBot.create_list(:random_shelter_review, 3, shelter: shelter) }
+shelter_1 = FactoryBot.create(:random_shelter)
+shelter_2 = FactoryBot.create(:random_shelter)
+shelter_3 = FactoryBot.create(:random_shelter)
+
+pets_1 = FactoryBot.create_list(:random_pet, 3, shelter: shelter_1)
+pets_2 = FactoryBot.create_list(:random_pet, 3, shelter: shelter_2)
+pets_3 = FactoryBot.create_list(:random_pet, 3, shelter: shelter_3)
+
+reviews_3 = FactoryBot.create_list(:random_shelter_review, 3, shelter: shelter_1)
+reviews_3 = FactoryBot.create_list(:random_shelter_review, 3, shelter: shelter_2)
+reviews_3 = FactoryBot.create_list(:random_shelter_review, 3, shelter: shelter_3)
+
+# seed 3 applications, each applying for a pet at a different shelter
+application_0 = FactoryBot.create(:application)
+application_0.pets << pets_1[0]
+application_0.pets << pets_2[0]
+application_0.pets << pets_3[0]
+application_1 = FactoryBot.create(:application)
+application_1.pets << pets_1[1]
+application_1.pets << pets_2[1]
+application_1.pets << pets_3[1]
+application_2 = FactoryBot.create(:application)
+application_2.pets << pets_1[2]
+application_2.pets << pets_2[2]
+application_2.pets << pets_3[2]
