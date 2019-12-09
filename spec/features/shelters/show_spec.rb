e1a49044b1a19cca77ccdb0d06ab06f9e74dfca3
Madelyn Romero <m.romero013@gmail.com>
2019-12-09 14:41:11 -0700

    Write test so cannot delete shelter if adoption pending on any pets

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -114,4 +114,17 @@ describe "As a visitor, when I visit /shelters/:id," do
     expect(page).not_to have_content(@reviews[0].content)
     expect(page).not_to have_content(@reviews[0].picture)
   end
+
+  it "cannot delete shelter if applications pending on any pets" do
+    shelter_1 = create(:random_shelter)
+    pet = create(:random_pet, shelter: :shelter_1)
+    pet.adoptable = false
+
+    visit "/shelters/#{shelter_1.id}"
+    click_link "Delete"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    expect(page).to have_content("Cannot delete #{shelter_1.name}, adoptions pending.")
+
+  end
 end
