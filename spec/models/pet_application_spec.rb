6904d601b78bff15c094fc26982d43331bc15558
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-09 17:52:23 -0700

    Add validation check for pending

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 describe PetApplication do
-  it {should belong_to :pet}
-  it {should belong_to :application}
+  describe "relationships" do
+    it {should belong_to :pet}
+    it {should belong_to :application}
+  end
+
+  describe "validations" do
+    it {should validate_presence_of :pending } 
+  end
 end
