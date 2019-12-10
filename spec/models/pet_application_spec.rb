6c1367ef9254798d52cb91c3899c3174e2800d10
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-09 22:02:16 -0700

    Add test for toggle_pending

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -8,6 +8,18 @@ describe PetApplication do
 
   describe "validations" do
     it {should validate_presence_of :pet_id} 
-    it {should validate_presence_of :application_id} 
+    it {should validate_presence_of :application_id}
+  end
+
+  describe "methods" do
+    it ".toggle_pending" do
+      app = create(:application)
+      pet = create(:random_pet)
+      app.pets << pet
+      pet_app = app.pet_applications.first
+      expect(pet_app.pending).to eq false
+      pet_app.toggle_pending
+      expect(pet_app.pending).to eq true
+    end
   end
 end
