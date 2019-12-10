e73772d561f454276999c9552599357b1cd4d480
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-09 19:35:42 -0700

    Remove pending validation, add pet and app id

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,5 +2,6 @@ class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
-  validates_presence_of :pending
+  validates_presence_of :pet_id
+  validates_presence_of :application_id
 end
