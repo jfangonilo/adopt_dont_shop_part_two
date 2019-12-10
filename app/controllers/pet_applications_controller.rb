2b329ac060f8b158aa7c9221a952759081429939
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-10 06:39:30 -0700

    Add to update action

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,3 +1,15 @@
 class PetApplicationsController < ApplicationController
-  
+  def update
+    app_id = params[:application_id]
+    pet_id = params[:pet_id]
+
+    application = Application.find(app_id)
+    pet = Pet.find(pet_id)
+    pet_app = PetApplication.where(pet_id: pet_id, application_id: app_id).limit(1).first
+
+    pet.toggle_adoptable
+    pet_app.toggle_pending
+
+    render "/pets/#{pet.id}"
+  end
 end
