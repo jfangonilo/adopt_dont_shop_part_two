aed578f1cf841dbe8593dd33bfad5fb51a24e414
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-10 09:42:27 -0700

    Remove test for removed feature

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -31,20 +31,4 @@ describe "When I visit /pets/:id," do
     expect(page).to_not have_content @pet.sex
     expect(page).to_not have_content @pet.description
   end
-
-  it "I can click a link to change the pet's status to Adoption Pending/Adoptable" do
-    click_link "Change to Adoption Pending"
-    expect(current_path).to eq "/pets/#{@pet.id}"
-    within "#adoptable-status" do
-      expect(page).to have_content "Adoption Pending"
-      expect(page).to_not have_content "Adoptable"
-    end
-
-    click_link "Change to Adoptable"
-    expect(current_path).to eq "/pets/#{@pet.id}"
-    within "#adoptable-status" do
-      expect(page).to_not have_content "Adoption Pending"
-      expect(page).to have_content "Adoptable"
-    end
-  end
 end
