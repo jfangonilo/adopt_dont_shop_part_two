55aab9aa1f2a2d82831b7a89fe658d32b756d4fb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-10 15:57:27 -0700

    Add delete application spec

---
+++ spec/features/applications/delete_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe "pets can't be deleted if they have an approved application;" do
+  it "page shows a flash message if pet is attempted to be deleted" do
+    pet = create(:random_pet)
+    app = create(:application)
+    app.pets << pet
+    pet.update(adoptable: false)
+    app.pet_applications.first.update(pending: true)
+
+    visit "/pets/#{pet.id}"
+    click_link "Delete"
+    expect(pet).to exist
+    expect(page).to have_content "Approved application pending. Cannot delete pet"
+  end
+end
