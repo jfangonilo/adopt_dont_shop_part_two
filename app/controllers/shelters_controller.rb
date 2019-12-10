759fc106ccb45fdc2ef2f8686457f689e3eee8f9
Madelyn Romero <m.romero013@gmail.com>
2019-12-09 18:56:30 -0700

    Delete code for not deleting test if has applications, need application approval to work first

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -28,19 +28,8 @@ class SheltersController < ApplicationController
   end
 
   def destroy
-    # shelter = Shelter.find(params[:id])
-    # pets = Shelter.find(params[:id]).pets
-    # pet_status = pets.map do |pet|
-    #   pet.adoptable?
-    # end
-    # adoption_pending = pet_status.include?(false)
-    # if adoption_pending
-    #   redirect_to "/shelters/#{shelter.id}"
-    #   flash[:notice] = "Cannot delete #{shelter.name}, adoptions pending."
-    # elsif adoption_pending == false
       Shelter.destroy(params[:id])
       redirect_to '/shelters'
-    # end
   end
 
 private
