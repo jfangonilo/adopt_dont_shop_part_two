522d0ddb16685d4881798ee1ae852f59612f962f
Madelyn Romero <m.romero013@gmail.com>
2019-12-11 14:07:12 -0700

    Refactor for boolean validation with activerecord

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -4,6 +4,7 @@ class PetApplication < ApplicationRecord
 
   validates_presence_of :pet_id
   validates_presence_of :application_id
+  validates :pending, inclusion: { in: [true, false]}
 
   def toggle_pending
     toggle! :pending
