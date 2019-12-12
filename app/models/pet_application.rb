72193b99e333877d248e7c71c90301d49ff0d311
Madelyn Romero <m.romero013@gmail.com>
2019-12-11 21:53:48 -0700

    Refactor validation

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -4,7 +4,7 @@ class PetApplication < ApplicationRecord
 
   validates_presence_of :pet_id
   validates_presence_of :application_id
-  validates :pending, inclusion: { in: [true, false]}
+  validates_inclusion_of :pending, in: [true, false]
 
   def toggle_pending
     toggle! :pending
