37110a69d38fdf363a267a4fb97956999de68de4
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-13 21:14:21 -0700

    Update apps controller to use favorites array

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,21 +2,18 @@ class ApplicationsController < ApplicationController
   before_action :require_pets, only: [:create]
 
   def new
-    @favorite_pets = favorites.contents.reduce([]) do |acc, (id, value)|
-      acc << Pet.find(id)
-    end
+    @favorite_pets = Pet.find(favorites.contents)
   end
 
   def create
     pet_ids = params[:pets_applied_for]
     pets = Pet.find(pet_ids)
     app = Application.new(application_params)
-
     if app.save
       favorites.delete_pets(pet_ids)
       app.pets << pets
-      redirect_to '/favorites'
       messages = pets.map { |pet| "Application sent for #{pet.name}!" }
+      redirect_to '/favorites'
       flash[:notice] = messages.join(' ')
     else
       redirect_to "/applications/new"
@@ -27,7 +24,6 @@ class ApplicationsController < ApplicationController
   def index
     pet = Pet.find(params[:pet_id])
     @applications = pet.applications
-
     if @applications.empty?
       flash.now[:notice] = "Show this one some love! No one has applied yet!"
     end
@@ -43,7 +39,7 @@ private
   end
 
   def require_pets
-    unless params[:pets_applied_for]
+    if !params[:pets_applied_for]
       flash[:notice] = "Please fill out all fields!"
       redirect_to "/applications/new"
     end
