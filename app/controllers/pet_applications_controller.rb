82be67b2627335ff1736fc3c8644e0054b12406f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-13 21:19:22 -0700

    Refactor for readability

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,19 +1,15 @@
 class PetApplicationsController < ApplicationController
   def update
-    app_id = params[:application_id]
     pet_id = params[:pet_id]
-
-    application = Application.find(app_id)
+    app_id = params[:application_id]
     pet = Pet.find(pet_id)
-    pet_app = PetApplication.where(pet_id: pet_id, application_id: app_id).first
-
     pet.toggle_adoptable
+    pet_app = pet.find_application(app_id)
     pet_app.toggle_pending
-
-    unless pet.adoptable?
-      redirect_to "/pets/#{pet.id}"
-    else
+    if pet.adoptable?
       redirect_to "/applications/#{app_id}"
+    else
+      redirect_to "/pets/#{pet_id}"
     end
   end
 end
