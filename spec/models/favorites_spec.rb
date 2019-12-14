ab496d53f0386df42dc9214971fdd20b8c9ecedb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-13 21:09:41 -0700

    Update favorites spec to use array

--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -7,19 +7,13 @@ RSpec.describe Favorites do
       list = Favorites.new
       list.add_pet(1)
 
-      expect(list.contents).to eq({'1' => 1})
+      expect(list.contents).to eq([1])
     end
   end
 
   describe "#total_count" do
     it "can calculate total number of pets in favorites" do
-      list = Favorites.new({
-        '1' => 1,
-        '2' => 1,
-        '3' => 1,
-        '4' => 1,
-        '5' => 1,
-        })
+      list = Favorites.new([1,2,3,4,5])
 
       expect(list.total_count).to eq(5)
     end
@@ -31,12 +25,12 @@ RSpec.describe Favorites do
       list.add_pet(1)
       list.delete_pets([1])
 
-      expect(list.contents).to eq({})
-      
+      expect(list.contents).to eq([])
+
       list.add_pet(1)
       list.add_pet(2)
       list.delete_pets([1,2])
-      expect(list.contents).to eq({})
+      expect(list.contents).to eq([])
     end
   end
 
