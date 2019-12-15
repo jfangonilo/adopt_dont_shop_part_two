e880c35a424d9ee144515247eab823e618ad193d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-15 14:33:33 -0700

    Move pet model clas methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :favorites
+  helper_method :favorites,
+                :all_pets_with_applications,
+                :all_pets_with_pending_applications
 
   def favorites
     @favorites = Favorites.new(session[:favorites] ||= [])
+  end
+
+  def all_pets_with_applications
+    Pet.all_with_applications
+  end
+
+  def all_pets_with_pending_applications
+    Pet.all_with_pending_application
   end
 end
